6007c88394d9962e50700cdeb6166c94264ee234
Denis Egorenko <degorenko@mirantis.com>
2015-12-02 19:28:50 +0300

    Remove using of ::sahara::config class
    This class was added in stable/liberty in upstream modules,
    but wasn't added to 7.0 Fuel release. Hence we should remove
    using of this class and use sahara_config resource directly.
    
    Change-Id: I6dd6671ecf84ff21dceb4a47300d3bb0197aac13
    Closes-bug: #1521663

--- deployment_scripts/sahara-volume-timeout.pp
+++ deployment_scripts/sahara-volume-timeout.pp
@@ -1,7 +1,5 @@
 notice('MODULAR: block-device/sahara-volume-timeout.pp')
 
-class { '::sahara::config':
-  sahara_config => {
-    'timeouts/await_attach_volumes' => { value => 60 },
-  }
+sahara_config {
+  'timeouts/await_attach_volumes': value => '60';
 }
