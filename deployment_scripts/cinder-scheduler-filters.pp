f7dd105c3056200fd05ab2c6f972d5a4a304a79a
Denis Egorenko <degorenko@mirantis.com>
2015-12-03 14:47:24 +0300

    Restart cinder services after changing cinder config on controllers
    Change-Id: I5f8324eb9639f9000b2eed40b8d64562a564eddd
    Closes-bug: #1522370

--- deployment_scripts/cinder-scheduler-filters.pp
+++ deployment_scripts/cinder-scheduler-filters.pp
@@ -1,5 +1,20 @@
 notice('MODULAR: block-device/cinder-scheduler-filters.pp')
 
+include ::cinder::params
+
 class { 'cinder::scheduler::filter':
   scheduler_default_filters => [ 'InstanceLocalityFilter', 'AvailabilityZoneFilter', 'CapacityFilter', 'CapabilitiesFilter' ],
 }
+
+service { 'cinder-api':
+  name      => $::cinder::params::api_service,
+  hasstatus => true,
+}
+
+service { 'cinder-scheduler':
+  name      => $::cinder::params::scheduler_service,
+  hasstatus => true,
+}
+
+Class['cinder::scheduler::filter'] ~> Service<| title == 'cinder-api' |>
+Class['cinder::scheduler::filter'] ~> Service<| title == 'cinder-scheduler' |>
